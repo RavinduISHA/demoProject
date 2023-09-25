b6a3f9c1bf1c20de7b5889828aa14f82b84c628f
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2023-09-25 17:15:13 +0530

    add authentication controller and make api route

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request){
+
+    }
+
+    public function login(LoginRequest $request){
+
+    }
+
+    public function logout(Request $request){
+
+    }
+}
